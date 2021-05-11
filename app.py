e52be6b385dbbf36dec986c011780ab93323aaff
Sunny Wang <wangjinlian@hotmail.com>
2021-05-11 11:21:32 -0400

    Implementation for login url endpoint working.

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, redirect
+import json
+import urllib.parse
 
 app = Flask(__name__)
-secret = "abcd"
+cognito_app_id = "1vvp0tt53g1uhntoa5bmvnvk2a"
+cognito_domain_prefix = "sunnyoauth"
+api_gateway_url = "https://fp6god49v7.execute-api.us-east-1.amazonaws.com/prod"
+
 
 @app.route('/api/authentication/login')
 def login_url():
-    return jsonify({"message": "Hello World2!"})
+    return redirect(getCognitoHost() + "/oauth2/authorize?client_id=" \
+                    + cognito_app_id + "&redirect_uri=" + urllib.parse.quote_plus(getRedirectURI()) \
+                    + "&scope=openid&response_type=code")
 
 
 @app.route('/api/authentication/status')
@@ -15,12 +22,21 @@ def login_status():
 
 @app.route('/api/authentication/exchange')
 def exchange_code():
-    return jsonify({"message": "Exchange for session token!"})
+    return json.dumps({"message": "Exchange for session token!"})
 
 
 @app.route('/api/authentication/logout')
 def logout():
     return jsonify({"message": "Logout!"})
+
+
+def getCognitoHost():
+    return "https://" + cognito_domain_prefix + ".auth.us-east-1.amazoncognito.com"
+
+
+def getRedirectURI():
+    return api_gateway_url + "/apis/authentication/exchange"
+
 
 if __name__ == '__main__':
     app.run()
