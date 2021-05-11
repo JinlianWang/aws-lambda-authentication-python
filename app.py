a9cb6e67c91c8c98b08474113d1d074dbb8d20b3
Sunny Wang <wangjinlian@hotmail.com>
2021-05-11 10:22:05 -0400

    Placeholder for 4 endpoints.

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
 from flask import Flask, jsonify
 
 app = Flask(__name__)
+secret = "abcd"
 
-@app.route('/')
-def hello_world():
-    return jsonify({"message": "Hello World!"})
+@app.route('/api/authentication/login')
+def login_url():
+    return jsonify({"message": "Hello World2!"})
+
+
+@app.route('/api/authentication/status')
+def login_status():
+    return jsonify({"message": "Status!" + secret})
+
+
+@app.route('/api/authentication/exchange')
+def exchange_code():
+    return jsonify({"message": "Exchange for session token!"})
+
+
+@app.route('/api/authentication/logout')
+def logout():
+    return jsonify({"message": "Logout!"})
 
 if __name__ == '__main__':
     app.run()
