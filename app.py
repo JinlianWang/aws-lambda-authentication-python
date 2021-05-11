72f3abde017f9c75bdbefa514fc09d0e5feff17c
Sunny Wang <wangjinlian@hotmail.com>
2021-05-11 11:44:07 -0400

    Implementation for status and logout endpoints, and corrected routes.

--- app.py
+++ app.py
@@ -1,33 +1,39 @@
 from flask import Flask, jsonify, redirect
 import json
 import urllib.parse
+from datetime import datetime, timezone, timedelta
 
 app = Flask(__name__)
 cognito_app_id = "1vvp0tt53g1uhntoa5bmvnvk2a"
-cognito_domain_prefix = "sunnyoauth"
+cognito_domain_prefix: str = "sunnyoauth"
 api_gateway_url = "https://fp6god49v7.execute-api.us-east-1.amazonaws.com/prod"
+sessionInfo = {"id": "123", "sub": "sunny", "expirationTime": datetime.now(timezone.utc) + timedelta(minutes=15)}
 
 
-@app.route('/api/authentication/login')
+@app.route('/apis/authentication/login')
 def login_url():
     return redirect(getCognitoHost() + "/oauth2/authorize?client_id=" \
                     + cognito_app_id + "&redirect_uri=" + urllib.parse.quote_plus(getRedirectURI()) \
                     + "&scope=openid&response_type=code")
 
 
-@app.route('/api/authentication/status')
+@app.route('/apis/authentication/status')
 def login_status():
-    return jsonify({"message": "Status!" + secret})
+    if sessionInfo is None:
+        return ""
+    return sessionInfo
 
 
-@app.route('/api/authentication/exchange')
+@app.route('/apis/authentication/exchange')
 def exchange_code():
     return json.dumps({"message": "Exchange for session token!"})
 
 
-@app.route('/api/authentication/logout')
+@app.route('/apis/authentication/logout')
 def logout():
-    return jsonify({"message": "Logout!"})
+    global sessionInfo
+    sessionInfo = None
+    return ""
 
 
 def getCognitoHost():
